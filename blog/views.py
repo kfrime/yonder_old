36ae3b603b4beca55416db0ec3d0eb7fd7542d59
kfrime <kfrime@163.com>
2018-05-05 13:57:51 +0800

    :art: show index page

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,17 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import (Tag, Topic, Article)
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        'tags': Tag.objects.all(),
+        'topics': Topic.objects.all().exclude(parent=None),
+        'articles': Article.objects.all(),
+    }
+    return render(request, 'index.html', context=context)
+
