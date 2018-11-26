6d79c7d9c767e2e641db8462f6d19b87ce1f4fe3
kfrime <kfrime@163.com>
2018-11-26 22:16:01 +0800

    添加webpack配置，让项目先运行起来

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
+}
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8020,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,   /* 显示错误信息 */
+    },
+    hot: true,          /* 修改某个组件后，只更新该组件的数据，不重新加载整个页面的全部数据 */
+  },
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config
