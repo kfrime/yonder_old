253a9e971190fb8c40aa0e1a70e9c09cf812aff5
kfrime <kfrime@163.com>
2018-12-01 00:32:26 +0800

    rename dir src to client

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
   target: 'web',
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: path.join(__dirname, 'client/index.js'),
   output: {
     filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, 'public')
@@ -47,7 +47,7 @@ const config = {
   },
   plugins: [
     new HTMLPlugin({
-      template: path.join(__dirname, 'src/template.html')
+      template: path.join(__dirname, 'client/template.html')
     })
   ]
 }
