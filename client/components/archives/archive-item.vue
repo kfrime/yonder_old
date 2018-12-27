3289124124ecc6e3f0eac37855fdb7f924368b75
kfrime <kfrime@163.com>
2018-12-27 23:29:21 +0800

    archive css

--- client/components/archives/archive-item.vue
+++ client/components/archives/archive-item.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="archive-item">
-    <div class="">
-      {{archive.year}}（共<span class="text-danger mx-1">{{archive.total}}</span>篇)
+  <div class="archive-item mb-2">
+    <div class="text-center archive-header mb-2">
+      <div class="mb-1 text-secondary">
+        {{archive.year}}年（共<span class="text-danger mx-1">{{archive.total}}</span>篇)
+      </div>
     </div>
     <ul>
-      <li v-for="art in archive.articles">
-        {{art.date}}&nbsp;
+      <li v-for="art in archive.articles" class="archive-article-link my-1">
+        <span class="text-secondary">{{art.date}}&nbsp;</span>
         <router-link
           :to="`/articles/${art.id}`"
           class="card-title card-link f-16 article-title"
@@ -22,6 +24,26 @@ export default {
 </script>
 
 <style>
+.archive-item {
+
+}
+.archive-header {
+  border-bottom: 2px solid #e9ecef;
+}
+.archive-article-link {
+
+}
+.archive-item ul {
+  list-style: none;
+}
+
+.archive-item ul li::before {
+  content: "\2022";
+  color: #6c757d;
+  display: inline-block;
+  width: 1em;
+  margin-left: -1em;
+}
 .article-title {
   color: #00a1d6;
 }
