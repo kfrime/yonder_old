60b079119fefd9597a891ce4fae2608be6ac9e4f
kfrime <kfrime@163.com>
2019-03-16 17:28:10 +0800

    add hello route

--- routes.go
+++ routes.go
@@ -9,10 +9,15 @@ func index(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world")
+}
+
 func initRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/hello", hello)
 
 	return mux
 }
