5504729bbb1473f8908926734fb2746c4810bb99
kfrime <kfrime@163.com>
2019-04-14 15:44:31 +0800

    add a simple test for redis

--- main.go
+++ main.go
@@ -3,52 +3,11 @@
 package main
 
 import (
-	"backend/debug"
-	"backend/model"
 	"backend/router"
-	"github.com/garyburd/redigo/redis"
 	"github.com/gin-gonic/gin"
 )
 
-func testUser()  {
-	db := model.DB
-
-	db.AutoMigrate(&model.User{})
-
-	// 创建
-	db.Create(&model.User{Name: "jack"})
-
-	// 读取
-	var user model.User
-	db.First(&user, 1)
-	db.First(&user, "name = ?", "jack")
-
-	// 更新
-	db.Model(&user).Update("Name", "bingo")
-
-	// 删除
-	//db.Delete(&user)
-}
-
-func testRedis()  {
-	rds := model.RedisPool.Get()
-	defer rds.Close()
-
-	if _, err := rds.Do("SET", "hello", "world"); err != nil {
-		dbg.Dbg(err)
-	}
-
-	s, err := redis.String(rds.Do("GET", "hello"))
-	if err != nil {
-		dbg.Dbg(err)
-	}
-	dbg.Dbg(s)
-}
-
 func main() {
-	//testUser()
-	testRedis()
-
 	// Creates a router without any middleware by default
 	app := gin.New()
 
---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"backend/debug"
+	"backend/model"
+	"github.com/garyburd/redigo/redis"
+	"testing"
+)
+
+func testUser()  {
+	db := model.DB
+
+	db.AutoMigrate(&model.User{})
+
+	// 创建
+	db.Create(&model.User{Name: "jack"})
+
+	// 读取
+	var user model.User
+	db.First(&user, 1)
+	db.First(&user, "name = ?", "jack")
+
+	// 更新
+	db.Model(&user).Update("Name", "bingo")
+
+	// 删除
+	//db.Delete(&user)
+}
+
+func TestRedis(t *testing.T)  {
+	rds := model.RedisPool.Get()
+	defer rds.Close()
+
+	if _, err := rds.Do("SET", "hello", "world"); err != nil {
+		dbg.Dbg(err)
+	}
+
+	got, err := redis.String(rds.Do("GET", "hello"))
+	if err != nil {
+		t.Errorf(`[GET] hello="%s"; err: %s`, got, err)
+	}
+	if got != "world" {
+		t.Errorf(`[GET] hello="%s"; want "world"`, got)
+	}
+}
