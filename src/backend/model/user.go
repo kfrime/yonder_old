57118fb08ed780d850d613ca124d0bdeef48c5e1
kfrime <kfrime@163.com>
2019-04-14 17:59:53 +0800

    check and encrypt user password

--- src/backend/model/user.go
+++ src/backend/model/user.go
@@ -1,6 +1,10 @@
 package model
 
 import (
+	"backend/config"
+	"backend/debug"
+	"crypto/md5"
+	"fmt"
 	"github.com/jinzhu/gorm"
 )
 
@@ -11,6 +15,23 @@ type User struct {
 	Role		int			`gorm:"not null"`
 }
 
+func (user User) validPasswd(passswd string) bool {
+	if passswd == "" {
+		return false
+	}
+	return true
+}
+
 // password md5 加密
+func (user User) EncryptPasswd(passwd string) (hashPasswd string) {
+	if !user.validPasswd(passwd) {
+		dbg.Dbg("password is not valid")
+	}
+
+	salt := config.AllConfig.Server.Salt
+	p := salt + passwd
+	hashPasswd = fmt.Sprintf("%x", md5.Sum([]byte(p)))
+	return
+}
 
 // user info to redis
