a37dee12d1f2cb7cda32846ea7b862d1cba02d5d
kfrime <kfrime@163.com>
2019-05-18 21:23:21 +0800

    config nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'stylesheet', href: '/styles/index.css' }
+    ]
+  }
+}
