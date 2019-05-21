5b66b6a7661b06dea6c3f7f4935522df84af1729
kfrime <kfrime@163.com>
2019-05-21 21:58:36 +0800

    change header style

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="app-header">
     <Menu mode="horizontal" :theme="menu_theme" @on-select="navRoute">
-      <div class="nav-header">
-
       <div class="app-logo">
         <a href="/">Yonder</a>
       </div>
-      <div class="app-header-right">
+      <!--<div class="app-header-left">-->
+      <div>
         <MenuItem name="home">Home</MenuItem>
         <MenuItem name="archive" >Archive</MenuItem>
         <MenuItem name="about">About</MenuItem>
-        <MenuItem name="signup">Sign up</MenuItem>
-        <MenuItem name="login">Login</MenuItem>
       </div>
+
       <div class="app-header-search">
-        <Input suffix="ios-search" placeholder="Enter text"/>
+        <Input suffix="ios-search" placeholder="search"/>
       </div>
+      <div class="app-header-right">
+        <MenuItem name="signup">Sign up</MenuItem>
+        <MenuItem name="login">Sign in</MenuItem>
       </div>
     </Menu>
   </div>
@@ -41,20 +42,16 @@
     position: relative;
     overflow: hidden;
   }
-  .nav-header {
-    /*margin: 0 15%;*/
-  }
   .app-logo {
     position: relative;
     float: left;
     color: rgba(255,255,255,.7);
     font-size: 20px;
-    padding: 0 20px;
+    padding: 0 40px;
   }
   .app-header-search{
     position: relative;
-    float: right;
-    display: inline;
+    float: left;
     /*position: center;*/
     padding: 0 20px;
   }
