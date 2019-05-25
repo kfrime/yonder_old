a8f3f9d1e0c29bd2fb8c48bbe1366bde1cb1e9ba
kfrime <kfrime@163.com>
2019-05-25 18:34:01 +0800

    Removal of Vuex store "classic mode"

--- store/index.js
+++ store/index.js
@@ -3,23 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const store = () => new Vuex.Store({
-  state: {
-    artilces: []
-  },
-  mutations: {
-    setArticles (state, articles) {
-      state.articles = articles
-    }
-  }
-})
+// const store = () => new Vuex.Store({
+//   state: {
+//     cates: [],
+//     articles: []
+//   },
+//   mutations: {
+//     setCates (state, cates) {
+//       state.cates = cates
+//     },
+//     setArticles (state, articles) {
+//       state.articles = articles
+//     }
+//   }
+// })
 
-export const state = () => {
-  return {
-    cates: [],
-    articles: []
-  }
-}
+export const state = () => ({
+  cates: [],
+  articles: []
+})
 
 export const mutations = {
   setCates (state, cates) {
