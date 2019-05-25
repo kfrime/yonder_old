f9df14a890a99494f2952d3306f35739b57753d6
kfrime <kfrime@163.com>
2019-05-25 09:50:59 +0800

    add col to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <app-header />
-    <nuxt />
+    <div class="app-container">
+      <!-- 间隔16px -->
+      <Row :gutter="16">
+        <!--<Col span="3">a</Col>-->
+        <Col span="16">
+          <nuxt />
+        </Col>
+        <Col span="8">
+          <app-sidebar />
+        </Col>
+        <!--<Col span="3">a</Col>-->
+      </Row>
+    </div>
   </div>
 </template>
 
 <script>
   import Header from '~/components/Header'
+  import Sidebar from '~/components/Sidebar'
 
   export default {
     components: {
-      'app-header': Header
+      'app-header': Header,
+      'app-sidebar': Sidebar
     }
   }
 </script>
+
+<style scoped>
+  .app-container {
+    width: 1200px;
+    margin: 20px auto;
+    min-height: 300px;
+  }
+</style>
