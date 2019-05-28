ae64d6acf3c63b756f0022ad3284c0d70761ccbf
kfrime <kfrime@163.com>
2019-05-28 22:53:22 +0800

    add page of article detail

--- pages/articles/_id.vue
+++ pages/articles/_id.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
-    article detail page: _id.vue
+    <div>
+      <Card dis-hover>
+        <Breadcrumb>
+          <BreadcrumbItem to="/">Home</BreadcrumbItem>
+          <!--todo: to category page-->
+          <BreadcrumbItem to="/">Components</BreadcrumbItem>
+          <BreadcrumbItem>Breadcrumb</BreadcrumbItem>
+        </Breadcrumb>
+      </Card>
+    </div>
+    <Card dis-hover>
+      <div slot="title">
+        <h1 class="article-title">{{article.Title}}</h1>
+        <div class="article-info">
+          <span>{{article.User.Name}}</span>
+          <span>{{article.CreatedAt}}</span>
+        </div>
+      </div>
+      <ButtonGroup slot="extra" v-if="isAdmin">
+        <Button :size="buttonSize"><Icon type="ios-code" /></Button>
+      </ButtonGroup>
+
+      <div>
+        {{article.Content}}
+      </div>
+
+      <div>pre article and next article</div>
+    </Card>
+    <!--todo: 添加文章目录 -->
   </div>
 </template>
 
@@ -8,6 +36,12 @@
   import request from '~/api/request'
 
   export default {
+    data () {
+      return {
+        buttonSize: "small",
+        isAdmin: this.$store.state.isAdmin,
+      }
+    },
     validate ({ params }) {
       // 必须是number类型
       return /^\d+$/.test(params.id)
@@ -18,19 +52,36 @@
       console.log("query:", ctx.query)
 
       return Promise.all([
-        request.getArticles({
+        request.getArticleDetail({
           client: ctx.req,
+          params: {
+            id: ctx.params.id
+          }
         })
       ]).then(resp => {
         console.log("get data:", resp)
+        let article = resp[0].data.ad || {}
 
         return {
-
+          article: article
         }
       }).catch(err => {
         console.log("catch error:", err)
         ctx.error({ message: "not found", statusCode: 404 })
       })
     },
+    layout: "nosidebar",
   }
 </script>
+
+<style scoped>
+  .article-title {
+    /*font-size: 30px;*/
+    /*font-weight: 700;*/
+    margin-bottom: 10px;
+    /*text-align: center;*/
+  }
+  .article-info {
+    /*margin: 5px 8px;*/
+  }
+</style>
