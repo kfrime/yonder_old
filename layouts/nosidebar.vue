356a2c4c6cf7969da8a253b7a70b49432ee07501
kfrime <kfrime@163.com>
2019-05-28 22:50:18 +0800

    add layout of nosidebar

---
+++ layouts/nosidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-body">
+    <app-header />
+    <div class="app-container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from '~/components/Header'
+
+  export default {
+    components: {
+      'app-header': Header,
+    }
+  }
+</script>
+
+<style scoped>
+  .app-body {
+    background:#eee;
+  }
+  .app-container {
+    background:#eee;
+    width: 1080px;
+    margin: 16px auto;
+    min-height: 300px;
+  }
+</style>
