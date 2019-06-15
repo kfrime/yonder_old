7615ddc82ac62822826c550ec6c5f2f42cc3749a
kfrime <kfrime@163.com>
2019-06-15 15:02:30 +0800

    add pagination to search api

--- src/backend/api/search.go
+++ src/backend/api/search.go
@@ -5,14 +5,49 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"strconv"
 )
 
+func paginate(x []SimpleArticle, skip int, size int) []SimpleArticle {
+	if skip > len(x) {
+		skip = len(x)
+	}
+
+	end := skip + size
+	if end > len(x) {
+		end = len(x)
+	}
+
+	return x[skip: end]
+}
+
 func SearchArticle(c *gin.Context)  {
+	var err error
+	var al []SimpleArticle
+	var page, limit int
+
+	pageStr := c.Query("page")
+	if pageStr == "" {
+		// 不传page参数，默认为1
+		page = 1
+	} else if page, err = strconv.Atoi(pageStr); err != nil {
+		log.Println(err)
+		SendErrResp(c, "param page no is not valid")
+		return
+	}
+
+	limitStr := c.Query("limit")
+	if limitStr == "" {
+		limit = 3
+	} else if limit, err = strconv.Atoi(limitStr); err != nil {
+		log.Println(err)
+		SendErrResp(c, "param limit is not valid")
+		return
+	}
+
 	q := c.Query("q")
 	// add % to q
 	fq := fmt.Sprintf("%%%s%%", q)
-	var al []SimpleArticle
-
 	var sql = `
 	SELECT a.id, a.title, a.created_at, a.updated_at, a.user_id, b.name as username, 
 	a.cate_id, c.name as cate_name
@@ -29,6 +64,7 @@ func SearchArticle(c *gin.Context)  {
 	}
 
 	SendResp(c, gin.H{
-		"al": al,
+		"al": paginate(al, (page - 1) * limit, limit),
+		"total": len(al),
 	})
 }
