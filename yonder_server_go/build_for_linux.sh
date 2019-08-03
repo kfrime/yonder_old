6fae89a8c295c1039ea108d60f7e2c62dd1de82c
kfrime <kfrime@163.com>
2019-08-03 17:10:34 +0800

    rename

--- yonder_server_go/build_for_linux.sh
+++ yonder_server_go/build_for_linux.sh
@@ -5,4 +5,4 @@ cur=`pwd`
 # export GOPATH=$GOPATH:${cur}
 export GOPATH=~/go:${cur}
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o yonder main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o yonder_server_go main.go
